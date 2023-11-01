d0a3b600ebcd5483945a4a3cfb5dec1fa1008603
Gabriel Gunawan <gabriel.gunawan@verint.com>
2023-11-01 19:11:23 +0700

    add authenticated route to home

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template, current_app #, session
+from flask import Blueprint, render_template, current_app , session
+
+from web_app.routes.wrappers import authenticated_route
 
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 @home_routes.route("/home")
+@authenticated_route
 def index():
     return render_template("home.html")
 
