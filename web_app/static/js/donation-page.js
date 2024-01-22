30c38d5c2ec4243567db8e1e9147e0255b0ead12
Gerry Caesario <gabriel.caesario@ist.id>
2024-01-23 06:15:58 +0700

    hide and show service charge and total

--- web_app/static/js/donation-page.js
+++ web_app/static/js/donation-page.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     $('input[type=radio][name=btnradio]').change(function() {
-        var value = this.value;
+        var value = parseInt($(this).val());
         var serviceChargeRate = $('#serviceChargeDiv :first-child').attr("data-service-charge-rate");
         var serviceCharge = value * serviceChargeRate
 
-        $('#serviceChargeDiv').toggle();
-        $('#totalDiv').toggle();
+        $('#serviceChargeDiv :nth-child(2)').text("$"+serviceCharge);
+
+        var totalPrice = serviceCharge + value;
+        $('#totalDiv :nth-child(2)').text("$"+totalPrice);
+
+        $('#serviceChargeDiv').show();
+        $('#totalDiv').show();
     });
 });
